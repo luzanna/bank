9e44d46260c5c1bf92bacc83d908618eedf09cc1
luzanna <58324018+luzanna@users.noreply.github.com>
2020-03-06 09:57:15 +0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+import Router from "vue-router";
+Vue.use(Router);
+
+
+ const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path:'/pay',
+      component: () => import('./components/Pay.vue'),
+    },
+      {
+          path:'/history',
+          component: () => import('./components/History.vue'),
+      },
+    {
+      path:'/successful_pay',
+      component: () => import('./components/Successful_Pay.vue'),
+    },
+    {
+        path: '/lk',
+      component: () => import('./components/Empty_Page.vue')
+    }
+  ],
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
