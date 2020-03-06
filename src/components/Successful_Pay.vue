d10ed1e718543d30a1ddb7cf76a472e6fd11adea
luzanna <58324018+luzanna@users.noreply.github.com>
2020-03-06 18:48:54 +0300

    Update Successful_Pay.vue

--- src/components/Successful_Pay.vue
+++ src/components/Successful_Pay.vue
@@ -23,6 +23,7 @@
                     </div>
                 </div>
             </div>
+            <button type="button" v-on:click="clearHistory" class="pay-button"> Очистить историю</button>
         </div>
         <div class="text-info">
             <p>
@@ -112,6 +113,13 @@
         },
         methods: {
 
+            // удаление истории транзакций
+            clearHistory(){
+                this.payment = null;
+                this.acct = null;
+                this.datePayment = null;
+            },
+
             // получение данных о транзакциях для вывода пользователю
             parseData(){
                 for (let [key, value] of Object.entries(this.formData)) {
@@ -131,11 +139,79 @@
         .pay-block{
             background: #f7f8f8;
             display: block;
-            width: 580px;
+
+            width: 70vw;
+            min-width: 580px;
+            max-width: 780px;
+
+            /*width: 50vw;*/
+            /*min-width: 580px;*/
             min-height: 565px;
             text-align: left;
             padding: 30px;
             font-size: small;
+        }
+        /*footer*/
+        .text-info{
+            background: #373c43;
+            width: 70vw;
+            min-width: 580px;
+            max-width: 780px;
+            padding: 30px;
+
+        }
+
+        .label-info{
+            margin-top: 15px;
+        }
+
+        .info{
+            margin-top: 5px;
+            color: #373c43;}
+
+        .table{
+            display: block;
+        }
+        .price{
+            padding-right: 30px;
+            text-align: right;
+        }
+
+        .tb{
+            display: inline-block;
+            width: 33%;
+            text-align: center;
+        }
+        .band{
+            margin-top: 15px;
+            margin-bottom: 15px;
+            height: 40px;
+            background: #e4e9ee;
+            width: 420px;
+            margin-left: -30px;
+
+        }
+
+        /*информация о карте*/
+        .card-data{
+            margin-top: 20px;
+            display: inline-block;
+            color: #8494ab;
+            border: 1px solid #e4e9ee;
+            border-radius: 10px;
+            padding: 30px;
+            width: 360px;
+        }
+
+
+        label{
+            color: #8494ab;
+        }
+
+        p{
+            color: #89909d;
+            text-align: left;
+            font-size: x-small;
         }
     }
 
@@ -143,71 +219,237 @@
         .pay-block{
             background: #f7f8f8;
             display: block;
-            width: 620px;
-            min-height: 565px;
+            width: 70vw;
+            min-width: 480px;
+            max-width: 580px;
             text-align: left;
-            padding: 10px;
+            padding: 30px;
             font-size: small;
+        }
+        /*footer*/
+        .text-info{
+            background: #373c43;
+            width: 70vw;
+            min-width: 480px;
+            max-width: 580px;
+            padding: 30px;
+
+        }
+
+
+        .label-info{
+            margin-top: 15px;
+        }
+
+        .info{
+            margin-top: 5px;
+            color: #373c43;}
+
+        .table{
+            display: block;
+        }
+        .price{
+            padding-right: 30px;
+            text-align: right;
+        }
+
+        .tb{
+            display: inline-block;
+            width: 33%;
+            text-align: center;
+        }
+        .band{
+            margin-top: 15px;
+            margin-bottom: 15px;
+            height: 40px;
+            background: #e4e9ee;
+            width: 420px;
+            margin-left: -30px;
+
+        }
+
+        /*информация о карте*/
+        .card-data{
+            margin-top: 20px;
+            display: inline-block;
+            color: #8494ab;
+            border: 1px solid #e4e9ee;
+            border-radius: 10px;
+            padding: 30px;
+            width: 360px;
+        }
+
+        label{
+            color: #8494ab;
+        }
+
+        p{
+            color: #89909d;
+            text-align: left;
+            font-size: x-small;
         }
 
     }
 
+    @media screen and (min-width: 480px){
+        .pay-block{
+            background: #f7f8f8;
+            display: block;
+            width: 95%;
+            /*height: 465px;*/
+            text-align: left;
+            padding: 20px;
+            font-size: small;
+        }
 
-    label{
-        color: #8494ab;
-    }
-    .label-info{
-        margin-top: 15px;
+        .label-info{
+            margin-top: 15px;
+        }
+
+        .info{
+            margin-top: 5px;
+            color: #373c43;}
+
+        .table{
+            display: block;
+        }
+        .price{
+            padding-right: 30px;
+            text-align: right;
+        }
+
+        .tb{
+            display: inline-block;
+            width: 33%;
+            text-align: center;
+        }
+        .band{
+            margin-top: 15px;
+            margin-bottom: 15px;
+            height: 40px;
+            background: #e4e9ee;
+            width: 420px;
+            margin-left: -30px;
+
+        }
+
+        /*информация о карте*/
+        .card-data{
+            margin-top: 20px;
+            display: inline-block;
+            color: #8494ab;
+            border: 1px solid #e4e9ee;
+            border-radius: 10px;
+            padding: 30px;
+            width: 360px;
+        }
+
+
+
+        /*footer*/
+        .text-info{
+            background: #373c43;
+            width: 95%;
+            padding: 20px;
+
+        }
+
+        label{
+            color: #8494ab;
+        }
+
+        p{
+            color: #89909d;
+            text-align: left;
+            font-size: x-small;
+        }
+
     }
 
-    .info{
-        margin-top: 5px;
-        color: #373c43;}
+    @media screen and (min-width: 320px){
+        .pay-block{
+            background: #f7f8f8;
+            display: block;
+            width: 96%;
+            /*height: 465px;*/
+            text-align: left;
+            padding: 7px;
+            font-size: small;
+        }
 
-    .table{
-        display: block;
-    }
-    .price{
-        padding-right: 30px;
-        text-align: right;
+        .label-info{
+            margin-top: 15px;
+        }
+
+        .info{
+            margin-top: 5px;
+            color: #373c43;}
+
+        .table{
+            display: block;
+        }
+        .price{
+            padding-right: 30px;
+            text-align: right;
+        }
+
+        .tb{
+            display: inline-block;
+            width: 33%;
+            text-align: center;
+        }
+        .band{
+            margin-top: 15px;
+            margin-bottom: 15px;
+            height: 40px;
+            background: #e4e9ee;
+            width: 420px;
+            margin-left: -30px;
+
+        }
+
+        /*информация о карте*/
+        .card-data{
+            margin-top: 20px;
+            display: inline-block;
+            color: #8494ab;
+            border: 1px solid #e4e9ee;
+            border-radius: 10px;
+            padding: 30px;
+            width: 360px;
+        }
+
+
+        /*footer*/
+        .text-info{
+            background: #373c43;
+            width: 96%;
+            padding: 7px;
+
+        }
+
+        label{
+            color: #8494ab;
+        }
+
+        p{
+            color: #89909d;
+            text-align: left;
+            font-size: x-small;
+        }
+
     }
 
-    .tb{
-        display: inline-block;
-        width: 33%;
-        text-align: center;
-    }
-    .band{
-        margin-top: 15px;
-        margin-bottom: 15px;
+    .pay-button{
+        background: #0054b2;
+        border-radius: 45px;
+        border: 0 solid;
+        color: #f4f6fa;
+        width: 135px;
         height: 40px;
-        background: #e4e9ee;
-        width: 420px;
-        margin-left: -30px;
-
-    }
-
-    /*информация о карте*/
-    .card-data{
-        margin-top: 20px;
-        display: inline-block;
-        color: #8494ab;
-        border: 1px solid #e4e9ee;
-        border-radius: 10px;
-        padding: 30px;
-        width: 360px;
-    }
-
-    .text-info{
-        background: #373c43;
-        width: 580px;
-        padding: 30px;
-
-    }
-    p{
-        color: #89909d;
-        text-align: left;
-        font-size: x-small;
+        font-size: xx-small;
+        margin-top: 30px;
+        margin-left: 45px;
     }
 
 </style>
